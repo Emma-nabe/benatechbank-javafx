33af2c6473cac7e3d9327e2dc714e05fa9394454
Emma-nabe <emmanuelnabe@gmail.com>
2024-01-23 10:18:35 +0100

    1- Connect to Database 2- Update Controller

--- src/main/java/com/btech/benatechbank/Controllers/Admin/CreateClientController.java
+++ src/main/java/com/btech/benatechbank/Controllers/Admin/CreateClientController.java
@@ -1,10 +1,7 @@
 package com.btech.benatechbank.Controllers.Admin;
 
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,7 +9,7 @@ import java.util.ResourceBundle;
 public class CreateClientController implements Initializable {
     public TextField fName_fld;
     public TextField lName_fld;
-    public TextField password_fld;
+    public PasswordField password_fld;
     public CheckBox pAddress_box;
     public Label pAddress_lbl;
     public CheckBox ch_acc_box;
--- src/main/java/com/btech/benatechbank/Controllers/LoginController.java
+++ src/main/java/com/btech/benatechbank/Controllers/LoginController.java
@@ -4,10 +4,7 @@ import com.btech.benatechbank.Models.Model;
 import com.btech.benatechbank.Views.AccountType;
 import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -17,7 +14,7 @@ public class LoginController implements Initializable {
     public ChoiceBox<AccountType> acc_selector;
     public Label payee_address_lbl;
     public TextField payee_address_fld;
-    public TextField password_fld;
+    public PasswordField password_fld;
     public Button login_btn;
     public Label error_lbl;
 
@@ -33,10 +30,20 @@ public class LoginController implements Initializable {
     private void onLogin()
     {
         Stage stage = (Stage) error_lbl.getScene().getWindow();
-        Model.getInstance().getViewFactory().closeStage(stage);
         if (Model.getInstance().getViewFactory().getLoginAccountType() == AccountType.CLIENT)
         {
-            Model.getInstance().getViewFactory().showClientWindow();
+            // Evaluer Client Credit Credential
+            Model.getInstance().evaluateClientCred(payee_address_fld.getText(), password_fld.getText());
+            if (Model.getInstance().getClientLoginSuccessFlag())
+            {
+                Model.getInstance().getViewFactory().showClientWindow();
+                // Close the login stage:
+                Model.getInstance().getViewFactory().closeStage(stage);
+            } else {
+                payee_address_fld.setText("");
+                password_fld.setText("");
+                error_lbl.setText("Informations erronees.");
+            }
         } else
         {
             Model.getInstance().getViewFactory().showAdminWindow();
